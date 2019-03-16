a47f0f862ef9e29c9f037e44b0b987e9289baef3
J-Zach Loke <jzloke@gmail.com>
2019-03-16 13:08:35 -0700

    reformatted output text

--- clicker.py
+++ clicker.py
@@ -84,6 +84,7 @@ def getpos(x, y, button, pressed):
     global mousepos
     if button == pynput.mouse.Button.middle and pressed:
         mousepos.append(mouse.position)
+        output()
         raise exit
 
 
@@ -125,30 +126,31 @@ def load():
 def output():
     os.system('cls')
     print('MOUSE CLICKER INFO')
-    print(f'file:         {file_name}') if file_name else print('file:         none')
-    print('status:       on') if on else print('status:       off')
-    print('mouse button: left') if mouse_button == pynput.mouse.Button.left else print('mouse button: right')
-    print(f'delay:        {delay} seconds')
-    print(f'click spots:  {mousepos}') if mousepos else print('click spots:  none')
+    print(f'file                   {file_name}') if file_name else print('file                   none')
+    print('status                 on') if on else print('status                 off')
+    print('mouse button           left') if mouse_button == pynput.mouse.Button.left else print('mouse button           right')
+    print(f'delay                  {delay} seconds')
+    print(f'click spots            {mousepos}') if mousepos else print('click spots            none')
     print('\n\n')
 
 
     print('CONTROLS')
-    print('backslash   starts and stops clicker')
-    print('delete      exits program')
+    print('backslash              starts and stops clicker')
+    print('delete                 exits program')
+    print('middle mouse button    adds click spots after entering the command \'add\'')
     print('\n\n')
 
     print('COMMANDS')
-    print('left                  change mouse button to left')
-    print('right                 change mousee button to right')
-    print('delay [number]        sets delay between clicks in seconds')
-    print('add                   adds mouse spots to click at')
-    print('remove [position]     removes mouse click spots at [position]')
-    print('resetpos              resets all mouse click spots')
-    print('save                  saves settings to current file')
-    print('save [file name]      saves settings to [file name]')
-    print('load [file name]      loads settings from [file name]')
-    print('resetfile             resets the file that\'s saved to and loaded from')
+    print('left                   change mouse button to left')
+    print('right                  change mousee button to right')
+    print('delay [number]         sets delay between clicks in seconds')
+    print('add                    adds mouse spots to click at')
+    print('remove [position]      removes mouse click spots at [position]')
+    print('resetpos               resets all mouse click spots')
+    print('save                   saves settings to current file')
+    print('save [file name]       saves settings to [file name]')
+    print('load [file name]       loads settings from [file name]')
+    print('resetfile              resets the file that\'s saved to and loaded from')
 
     print('\n\n\n')
     print('AWAITING COMMANDS...')
@@ -181,7 +183,6 @@ while True:
 
         # mouse positions
         elif inp == 'add':
-            print('press middle mouse button to get mouse position')
             pos_thread = threading.Thread(target=pos_listen)
             pos_thread.daemon = True
             pos_thread.start()
